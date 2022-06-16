1d61d332b19cb3ea6f0c08c2065653b39a10f177
PedroGont <pedrotolentino2002@gmail.com>
2022-06-15 21:28:17 -0700

    Problem7 A3

--- WMAD2020-Structure/wmad202-assignment3/src/ca/ciccc/wmad202/assignment3/main/Assignment3Driver.java
+++ WMAD2020-Structure/wmad202-assignment3/src/ca/ciccc/wmad202/assignment3/main/Assignment3Driver.java
@@ -6,6 +6,7 @@ import ca.ciccc.wmad202.assignment3.problem3.Problem3;
 import ca.ciccc.wmad202.assignment3.problem4.Problem4;
 import ca.ciccc.wmad202.assignment3.problem5.Problem5;
 import ca.ciccc.wmad202.assignment3.problem6.Problem6;
+import ca.ciccc.wmad202.assignment3.problem7.Problem7;
 
 public class Assignment3Driver {
     public static void run() {
@@ -19,7 +20,9 @@ public class Assignment3Driver {
         //question4.TakeNumbers();
         //Problem5 question5 = new Problem5();
         //question5.TakeNumbers();
-        Problem6 question6 = new Problem6();
-        question6.TakeNumbers();
+        //Problem6 question6 = new Problem6();
+        //question6.TakeNumbers();
+        Problem7 question7 = new Problem7();
+        question7.TakeNumbers();
     }
 }
