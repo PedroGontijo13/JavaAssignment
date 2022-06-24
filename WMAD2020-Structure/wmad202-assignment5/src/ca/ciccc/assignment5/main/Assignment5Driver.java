e42cfb1c63a96f3a275ac1d1887bde73589c38b0
PedroGont <pedrotolentino2002@gmail.com>
2022-06-23 20:38:34 -0700

    Problem1 Solved

--- WMAD2020-Structure/wmad202-assignment5/src/ca/ciccc/assignment5/main/Assignment5Driver.java
+++ WMAD2020-Structure/wmad202-assignment5/src/ca/ciccc/assignment5/main/Assignment5Driver.java
@@ -1,10 +1,13 @@
 package ca.ciccc.assignment5.main;
 
+import ca.ciccc.assignment5.question1.Question1;
 import ca.ciccc.assignment5.question2.Question2;
 
 public class Assignment5Driver {
     public static void run() {
-        Question2 question2 = new Question2();
-        question2.invoke();
+        Question1 question1 = new Question1();
+        question1.invoke();
+        //Question2 question2 = new Question2();
+        //question2.invoke();
     }
 }
