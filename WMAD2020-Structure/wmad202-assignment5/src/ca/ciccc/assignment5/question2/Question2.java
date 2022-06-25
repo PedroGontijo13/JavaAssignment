fb80e6cb32f2e8414938a55effecd0ccf8054241
PedroGont <pedrotolentino2002@gmail.com>
2022-06-24 19:39:55 -0700

    Problem 2.3 OK

--- WMAD2020-Structure/wmad202-assignment5/src/ca/ciccc/assignment5/question2/Question2.java
+++ WMAD2020-Structure/wmad202-assignment5/src/ca/ciccc/assignment5/question2/Question2.java
@@ -3,7 +3,8 @@ package ca.ciccc.assignment5.question2;
 public class Question2 {
     public void invoke(){
         //TestProblem2_1();
-        TestProblem2_2();
+        //TestProblem2_2();
+        TestProblem2_3();
     }
 
     public void TestProblem2_1() {
@@ -24,5 +25,14 @@ public class Question2 {
         Pages pages1 = new Pages("LOREM LOREM");
         book.addPage(pages1);
         book.CheckWords("LOREM");
+    }
+
+    public void TestProblem2_3() {
+        Transaction t = new Transaction("999" , 2.02F);
+        BankStatment b = new BankStatment();
+        b.addTransction(t);
+        Transaction t2 = new Transaction("999" , 2.02F);
+        b.addTransction(t2);
+        b.totalBalance();
     }
 }
