524928a7252a5ae03a73fe60dd230b54b900ba0b
PedroGont <pedrotolentino2002@gmail.com>
2022-07-15 12:13:39 -0700

    Assignment7 OK

--- WMAD2020-Structure/wmad202-assignment7/src/ca/ciccc/assignment7/problem4/Test4.java
+++ WMAD2020-Structure/wmad202-assignment7/src/ca/ciccc/assignment7/problem4/Test4.java
@@ -3,8 +3,6 @@ package ca.ciccc.assignment7.problem4;
 import java.util.Arrays;
 
 public class Test4 {
-
-
     public void main() {
         Service service = new IService();
         service.addItemToList(Arrays.asList(1,2,3,4,5));
